9b1bb3c4ce88fcb777fb230b88801f99102f8d8c
Alireza Valikhani <alirezavalikhabi1998@gmail.com>
2022-07-25 12:24:08 +0430

    Basic classes and files of project

---
+++ src/main/java/com/test/mongodbDocker/MongodbDockerApplication.java
@@ -0,0 +1,13 @@
+package com.test.mongodbDocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongodbDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/mongodbDocker/MongodbDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.mongodbDocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongodbDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
